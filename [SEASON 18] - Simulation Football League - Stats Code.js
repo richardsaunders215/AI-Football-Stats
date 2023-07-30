a1417a4a2e1a74c3bee5186f93d4901c96c33c58
Richard <9585510+richardsaunders215@users.noreply.github.com>
2023-07-30 17:32:02 +0100

    Add files via upload

---
+++ [SEASON 18] - Simulation Football League - Stats Code.js
@@ -0,0 +1,101 @@
+// ROSCA SANTIGRIA - Simulation Football League Stats
+
+// GOAL - To create a JAVASCRIPT coded version of my SFL Football Stats.
+
+// Hype Huskies - Season 2 Episode 16
+
+
+const playerInformation = {
+
+    playerName: "Rosca Santigria",
+    playerRank: "Gold",
+    playerOverall: "78.46",
+    season: "SFL SEASON 18",
+    legendsFace: "Walter Payton",
+
+    position: "Cornerback",
+    jerseyNumber: 35,
+    starPlayer: "YES",
+    weight: 218,
+    height: "6 ft 1 in",
+   
+
+}
+
+
+// playerStats: The stats my SFL player
+
+const playerStats = {
+speed: 92,
+agility: 92,
+stamina: 87,
+strength: 62,
+jump: 90,
+coverage: 90, 
+aggressiveness: 69,
+tackle: 70, 
+catch: 40,
+passRush: 20,
+runCoverage: 45, 
+consistency: 90,
+}
+
+
+/* highValueAnimation: Explains the animatuions my SFL player has. 
+
+Bump Master = Bump Master is an animation that allows the Cornerback
+to bump agains the Wide Receiver. This slows them down and leaves
+them open for a pass deflection or interception.
+
+*/
+
+const highValueAnimations = {
+    animationOne: "Bump Master",
+
+}
+
+
+// PLAYER EXTRA DETAILS
+
+const playerExtraDetails = {
+    age: 58,
+    face: "FACE1",
+    hand: "RIGHT",
+    leftGlove: "WHOLE",
+    leftShoeTape: "FALSE",
+
+    muscleType: "RIPPED",
+    rightShoe: "COLOR 3",
+    runStyle: "FINESSE",
+    turtleNeck: "NONE",
+
+    bodyType: "NORMAL",
+    faceShield: "DARK",
+    kickStyle: "TRADITIONAL",
+    leftGloveColor: "COLOR 3",
+    leftWrist: "NONE",
+    
+    nasalStrip: "FALSE",
+    rightGlove: "WHOLE",
+    rightShoeTape: "FALSE",
+    skinTone: "DARK",
+
+
+    eyeBlack: "FALSE",
+    faceMask: "TYPE 13",
+    leftElbow: "NONE",
+    leftShoe: "COLOR 3",
+    legPads: "LIGHT",
+
+    neckRoll: "NONE",
+    rightGloveColor: "COLOR 3",
+
+}
+
+
+console.log(playerInformation)
+console.log(playerStats)
+console.log(highValueAnimations)
+console.log(playerExtraDetails)
+
+
